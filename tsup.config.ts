85399285b2cbd37da1e225dc2c17d2e8de491c17
Christos Syrmakezis <sirmchristos@gmail.com>
2023-07-14 18:00:23 +0300

    Fixing issues with missing schemas

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   splitting: false,
+  treeshake: false,
   format: ["cjs"],
   dts: true,
   minify: true,
-  sourcemap: "inline",
+  sourcemap: true,
+  skipNodeModulesBundle: true,
   clean: true,
   target: "es2020",
   external: ["zod"],
